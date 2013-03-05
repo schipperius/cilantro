b03e7a90d15cb5ad8e08f23a34887c1a6dc7a446
schipperius <schipperius@gmail.com>
2013-03-04 20:36:05 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cilantro::Application.config.secret_token = 'f37cd254ce3461e1bf4d12777b574ee4894e59a0ae7d32d2e7b4d2a3e1cab75eeb2eefe5b501215b7dc7e8cb30ceb4d490cabad6146e295e5a591eca3e9af430'
